8a002b82b8c49fc6606bc761e3c29cfb284b40ed
Mirco Limpinsel-Pesavento <122088954+lmpnsl@users.noreply.github.com>
2025-06-29 22:15:29 +0200

    Create KalenderwochenSortieren.js

---
+++ actions/KalenderwochenSortieren.js
@@ -0,0 +1,17 @@
+let text = draft.content;
+
+function sortKalenderwochen(input) {
+    const regex = /(KW \d+[\s\S]*?(?=(KW \d+|$)))/g;
+    const wochen = [...input.matchAll(regex)].map(match => match[0]);
+
+    const sortedWochen = wochen.sort((a, b) => {
+        const numA = parseInt(a.match(/KW (\d+)/)[1], 10);
+        const numB = parseInt(b.match(/KW (\d+)/)[1], 10);
+        return numA - numB;
+    });
+
+    return sortedWochen.join("\n\n");
+}
+
+draft.content = sortKalenderwochen(text);
+draft.update(); // Speichert die Änderungen in der Notiz
